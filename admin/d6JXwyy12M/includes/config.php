d638f7f3a37d0e2202c02a5468877832bc1c4d31
DecorSS <kerimovvusal65@gmail.com>
2025-09-27 15:20:14 +0400

    Update config.php

--- admin/d6JXwyy12M/includes/config.php
+++ admin/d6JXwyy12M/includes/config.php
@@ -8,8 +8,8 @@ require_once __DIR__ . '/security.php';
 setSecurityHeaders();
 
 $dsn = 'mysql:host=13.61.139.31;port=3306;dbname=decorss;charset=utf8mb4';
-$dbUser = 'root';
-$dbPass = '@Bura-Girme1991';
+$dbUser = 'render_user';
+$dbPass = 'Girme1991-@Bura';
 
 $options = [
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
