4194f32a700ca228b7ae8048af6c44f16ceda5a8
DecorSS <kerimovvusal65@gmail.com>
2025-09-27 15:27:18 +0400

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -10,9 +10,10 @@
     <meta property="og:image" content="assets/images/logo_icon/ss_ganja_logo.png">
     <meta name="keywords" content="dekor, gəncə dekor, nişan dekoru, toy dekoru">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 <body>
     <button class="menu-toggle">☰</button>
     <?php include __DIR__ . '/navbar.php'; ?>
+
 
