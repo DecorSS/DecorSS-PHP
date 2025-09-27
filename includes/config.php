30804aa99d29b26d7e0523ce718c5d6525607673
DecorSS <kerimovvusal65@gmail.com>
2025-09-27 14:55:30 +0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // Güvenlik fonksiyonlarını dahil et
 require_once __DIR__ . '/../admin/d6JXwyy12M/includes/security.php';
 
-$dsn = 'mysql:host=127.0.0.1;port=3306;dbname=decorss;charset=utf8mb4';
+$dsn = 'mysql:host=13.61.139.31;port=3306;dbname=decorss;charset=utf8mb4';
 $dbUser = 'root';
 $dbPass = '@Bura-Girme1991';
 
